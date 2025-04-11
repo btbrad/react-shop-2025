2080535b869efe3250893be9d698c4d00b636e56
btbrad <biantaobrad@gmail.com>
2025-04-11 22:25:25 +0800

    fix: 移动端1px边框

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div>happy shop</div>
+  return (
+    <>
+      <div style={{ borderBottom: '1px solid #ccc' }}>
+        下面的底边宽度是虚拟1像素的
+      </div>
+    </>
+  )
 }
 
 export default App
