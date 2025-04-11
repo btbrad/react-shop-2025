2bc5b4a90e8452f40efbbdbe14052ff35145250c
btbrad <biantaobrad@gmail.com>
2025-04-11 22:59:48 +0800

    rem适配

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import './App.css'
+
 function App() {
   return (
     <>
-      <div style={{ borderBottom: '1px solid #ccc' }}>
-        下面的底边宽度是虚拟1像素的
-      </div>
+      <div className="test">hello react</div>
     </>
   )
 }
