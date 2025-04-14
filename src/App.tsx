fbebcc38fe99caae1655fe589ad14bf7b41e0bb7
btbrad <biantaobrad@gmail.com>
2025-04-14 22:14:39 +0800

    feat: 启动页样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import './App.css'
+import './styles/App.css'
+
+import logo from './assets/react.svg'
+import { useRef, useEffect } from 'react'
 
 function App() {
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const el = ref.current
+    if (el) {
+      el.style.opacity = '1'
+    }
+  }, [])
+
   return (
-    <>
-      <div className="test">hello react</div>
-    </>
+    <div ref={ref} className="page guide-page">
+      <img className="main-pic" src={logo} alt="logo" />
+      <div className="title">欢乐购</div>
+      <div className="slogan1">买的开心</div>
+      <div className="slogan2">吃的放心</div>
+      <div className="arrow-right iconfont">&#xe624;</div>
+    </div>
   )
 }
 
