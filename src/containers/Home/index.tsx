5a5fb710016bb5a6c234436fca6193ed4c401edb
btbrad <biantaobrad@gmail.com>
2025-11-04 15:20:05 +0800

    fix: 获取位置信息

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,8 +1,34 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper.css'
 import './index.scss'
+import { useEffect } from 'react'
 
 const Home = () => {
+  useEffect(() => {
+    const localPosition = window.localStorage.getItem('position')
+    if (localPosition) {
+      console.log('本地缓存位置：', JSON.parse(localPosition))
+      return
+    }
+
+    if (window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position)
+          const { longitude, latitude } = position.coords
+          console.log(`纬度：${latitude} 经度：${longitude}`)
+          window.localStorage.setItem(
+            'position',
+            JSON.stringify({ longitude, latitude })
+          )
+        },
+        (err) => {
+          console.log(err)
+        }
+      )
+    }
+  }, [])
+
   return (
     <div>
       <h1>Home</h1>
