600a74bf9990eb2a0329a68bb14205554d615871
btbrad <biantaobrad@gmail.com>
2025-11-04 14:53:34 +0800

    首页轮播图

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,7 +1,27 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper.css'
+import './index.scss'
+
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <div className="banner-item">Slide 1</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="banner-item">Slide 2</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="banner-item">Slide 3</div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   )
 }
